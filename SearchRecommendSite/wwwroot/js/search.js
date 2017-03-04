e6e427697d6ef0567cf535b05501c995657db1ba
abalter <avishay.balter@gmail.com>
2017-03-04 12:51:04 +0200

    adding bot

--- SearchRecommendSite/wwwroot/js/search.js
+++ SearchRecommendSite/wwwroot/js/search.js
@@ -1,7 +1,7 @@
-var azureSearchQueryApiKey = "54D83159A5E23BBF0AA35349F4DF1B31";
-var azureSearchApiManageKey = "5a117587e47b4cb199e9b78caf8ef7ac"
-var azureMLModelId = "45446586-493f-40e2-9d1a-a7bd9853375d"
-var azureMLBuildId = "1543796"
+//var azureSearchQueryApiKey = "54D83159A5E23BBF0AA35349F4DF1B31";
+var azureSearchApiManageKey = "424a7e57dcab40e4a3d33c64459b4552"
+//var azureMLModelId = "45446586-493f-40e2-9d1a-a7bd9853375d"
+//var azureMLBuildId = "1543796"
 var inSearch = false;
 
 function execSuggest()
@@ -79,18 +79,19 @@ function openMovieDetails(title, hebtitle, id, description)
 
 	$("#recDiv").html('Loading recommendations...');
 
-	var recommendatationAPI = "https://api.datamarket.azure.com/data.ashx/amla/recommendations/v2/ItemRecommend?$format=json&modelId='" + azureMLModelId + "'&numberOfResults=5&buildId=" + azureMLBuildId + "&includeMetadata=false&apiVersion='1.0'&itemIds='" + id + "'";
+    var recommendatationAPI = "https://tspapimanagement.azure-api.net/RecommendApi/Recommend/recommendId=" + id;
 
 	$.ajax({
 		url: recommendatationAPI,
 		beforeSend: function (request) {
-			request.setRequestHeader("Authorization", "Basic QWNjb3VudEtleTpIK0ZDVldETWZZbnpja2ZUa3pxNDlzT01aR2dFVDlVNFdqL2xCSHhZeStzPQ==");
+            request.setRequestHeader("Ocp-Apim-Subscription-Key", azureSearchApiManageKey);
+            request.setRequestHeader("Content-Type", "application/json");
 		},
 		type: "GET",
 		success: function (data) {
 			$("#recDiv").html('');
-			for (var item in data.d.results)
-				$( "#recDiv" ).append( '<p>' + data.d.results[item].Name + '</p>' );
+            for (var item in data.recommendedItems)
+                $("#recDiv").append('<p>' + data.recommendedItems[item].items[0].name + '</p>' );
 		}
 	});
 
